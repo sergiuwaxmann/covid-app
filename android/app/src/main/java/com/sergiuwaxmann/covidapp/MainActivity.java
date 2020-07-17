7761e7ea9c8fdc53c4bd286d762bad1ca01829d5
Sergiu Waxmann <waxmann.sergiu@me.com>
2020-07-17 17:20:50 +0300

    Initial commit

---
+++ android/app/src/main/java/com/sergiuwaxmann/covidapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.sergiuwaxmann.covidapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CovidApp());
+
+class CovidApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+  }
+}
