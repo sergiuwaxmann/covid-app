68f822a2fa15e6df4a91164e271534665e72711d
Sergiu Waxmann <waxmann.sergiu@me.com>
2020-07-18 16:48:45 +0300

    Main header

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:covidapp/constant.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(CovidApp());
 
 class CovidApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'COVID-19',
+        theme: ThemeData(
+            scaffoldBackgroundColor: kBackgroundColor,
+            fontFamily: 'Poppins',
+            // ignore: deprecated_member_use
+            textTheme: TextTheme(body1: TextStyle(color: kBodyTextColor))),
+        home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              height: 350,
+              width: double.infinity,
+              padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [Color(0xFF3383CD), Color(0xFF11249F)],
+                ),
+                image: DecorationImage(
+                    image: AssetImage('assets/images/virus.png')),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                      alignment: Alignment.topRight,
+                      child: SvgPicture.asset('assets/icons/information.svg')),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                    child: Stack(
+                      children: <Widget>[
+                        SvgPicture.asset(
+                          'assets/icons/doctor1.svg',
+                          width: 230,
+                          fit: BoxFit.fitWidth,
+                          alignment: Alignment.topCenter,
+                        ),
+                        Positioned(
+                          left: 150,
+                          top: 20,
+                          child: Text(
+                            'All you need\nis stay at home.',
+                            style: kHeadingTextStyle.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Container(), // I don't know why it doesn't work without it
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
